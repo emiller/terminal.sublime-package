8b8ed9d15476fbf6c3e99805c3bfa88f6da6bb5f
emiller <emiller@playdom.com>
2013-03-19 16:30:33 -0700

    Adding in Terminal.sublime-package plugin

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-Terminal.sublime-plugin
-=======================
+## Terminal.sublime-plugin
 
 Sublime plugin that adds an "Open Terminal Here.." utility.
+
+### Installation
+
+Just drop the compressed `Terminal.sublime-package` package into Sublime's
+`Installed Packages` directory, for example:
+
+```bash
+cp Terminal.sublime-package  ~/.config/sublime-text-2/Installed\ Packages/
+```
+
+..and then restart Sublime. 
+
+### Usage
+
+After installation you'll a new menu entry:
+
+    Goto -> Open Terminal Here (CTRL+T)
+    
+While in your editor you can use the hotkey `CTRL+T` (or `Command+T`) to
+open a terminal at the location of the current file being edited.
+
+***
+
+_emiller_ _2013-03-19_
+
---
+++ Terminal/open_terminal.py
@@ -0,0 +1,37 @@
+"""
+@author       emiller
+@date         01/22/2013
+@description  Simple Sublime plugin that enables the familiar
+              Open Terminal Here support within the editor.
+"""
+
+import sublime, sublime_plugin, os, sys
+
+class OpenTerminalCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    path = os.path.dirname(self.view.file_name())
+
+    if not os.path.exists(path):
+      sublime.error_message('Hmm, that path doesn\'t exist anymore.')
+      return
+
+    platform = sys.platform.lower()
+    terminal = None
+
+    if platform.startswith('linux'):
+      terminal = 'x-terminal-emulator'
+
+    elif platform.startswith('osx'):
+      terminal = 'open -a "Terminal"'
+
+    if terminal is None:
+      sublime.message_dialog('Sorry, only Linux and OSX are supported currently.') 
+      return
+
+    try:
+      os.system('cd %s; %s &' % (path, terminal))
+
+      #os.system('nohup %s --working-directory "%s" >/dev/null 2>&1 &' % (terminal, path))
+
+    except Exception, e:
+      sublime.error_message('Unable to open terminal (%s) because: %s' % (terminal, str(e)))
